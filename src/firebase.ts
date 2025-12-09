0f86a608bc9c04b0613798ee2485ee49529e6bb0
JohnHuang626 <make626@jsjh.cyc.edu.tw>
2025-12-09 17:48:02 +0800

    完成運動會網站

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA8N_mCRjfCtXB97OpIsiyVHds-bxOmUso',
-  authDomain: 'jiashin-sports-day.firebaseapp.com',
-  projectId: 'jiashin-sports-day',
-  storageBucket: 'jiashin-sports-day.firebasestorage.app',
-  messagingSenderId: '758992182792',
-  appId: '1:758992182792:web:06fc7f9a00ad322a023bbd',
+  apiKey: "AIzaSyA8N_mCRjfCtXB97OpIsiyVHds-bxOmUso",
+  authDomain: "jiashin-sports-day.firebaseapp.com",
+  projectId: "jiashin-sports-day",
+  storageBucket: "jiashin-sports-day.firebasestorage.app",
+  messagingSenderId: "758992182792",
+  appId: "1:758992182792:web:06fc7f9a00ad322a023bbd"
 };
 
 const app = initializeApp(firebaseConfig);
